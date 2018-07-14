aac2d907e552d06c43a806be86ef8f8b49719fec
Michael Abdulai <michael@a-boss.net>
2018-07-15 00:09:39 +0200

    yes

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,56 @@
-import React, { Component } from 'react';
-import UserStats from './user-stats';
-import './App.css';
+import React, { Component } from "react";
+import UserStats from "./user-stats";
+import "./App.css";
 
 class App extends Component {
   state = {
     mika: {},
     thomas: {},
     mikaLoading: true,
-    thomasLoading: true,
-  }
+    thomasLoading: true
+  };
   componentDidMount() {
-    fetch('/mika')
+    fetch("/mika")
       .then(res => res.json())
       .then(data => {
         const user = JSON.parse(data);
-        console.log('mika', user);
-        this.setState({mikaLoading: false, mika: {solo: user.stats.curr_p2, duo: user.stats.curr_p10, squads: user.stats.curr_p9}})
+        console.log("mika", user);
+        this.setState({
+          mikaLoading: false,
+          mika: {
+            solo: user.stats.curr_p2 = 0,
+            duo: user.stats.curr_p10 = 0,
+            squads: user.stats.curr_p9 = 0
+          }
+        });
       });
-    fetch('/thomas')
+    fetch("/thomas")
       .then(res => res.json())
       .then(data => {
         const user = JSON.parse(data);
-        console.log('thomas', user);
-        this.setState({thomasLoading: false, thomas: {solo: user.stats.curr_p2, duo: user.stats.curr_p10, squads: user.stats.curr_p9}})
+        console.log("thomas", user);
+        this.setState({
+          thomasLoading: false,
+          thomas: {
+            solo: user.stats.curr_p2 = 0,
+            duo: user.stats.curr_p10 = 0,
+            squads: user.stats.curr_p9 = 0
+          }
+        });
       });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        ABOSSNITE
-        </header>
+        <header className="App-header">ABOSSNITE</header>
         <main>
-          {!this.state.mikaLoading && <UserStats stats={this.state.mika} user="Mika"/> }
-          {!this.state.thomasLoading && <UserStats stats={this.state.thomas} user="Thomas"/> }
+          {!this.state.mikaLoading && (
+            <UserStats stats={this.state.mika} user="Mika" />
+          )}
+          {!this.state.thomasLoading && (
+            <UserStats stats={this.state.thomas} user="Thomas" />
+          )}
         </main>
       </div>
     );
